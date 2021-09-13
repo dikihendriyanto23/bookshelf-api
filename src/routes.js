ff950909a35fd59728686498857175f06893a81c
Muhammad Diki Hendriyanto <49431235+dikihendriyanto23@users.noreply.github.com>
2021-09-13 15:10:15 +0700

    Add files via upload

---
+++ src/routes.js
@@ -0,0 +1,44 @@
+const {
+  addBooksHandler,
+  getAllBooksHandler,
+  getBookByIdHandler,
+  editBookByIdHandler,
+  deleteBookByIdHandler
+} = require('./handler')
+
+const routes = [
+  {
+    method: 'GET',
+    path: '/',
+    handler: () => {
+      return 'Ini adalah halaman home'
+    }
+  },
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBooksHandler
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler
+  },
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: getBookByIdHandler
+  },
+  {
+    method: 'PUT',
+    path: '/books/{id}',
+    handler: editBookByIdHandler
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{id}',
+    handler: deleteBookByIdHandler
+  }
+]
+
+module.exports = routes
